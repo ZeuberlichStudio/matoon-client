7dc8ff0f93e66467e61b3b5ddcc569a9a67f3603
Yaroslav <goooglemaaan@gmail.com>
2020-10-26 19:16:01 +0300

    changed import from app alias

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import store from './app/store';
+import store from 'app/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
