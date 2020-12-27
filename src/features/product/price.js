a217d8eb78f1a8e6af4b151686ac3a7c2078260c
Yaroslav Dymov <dymov.yar@gmail.com>
2020-12-27 18:28:56 +0300

    fixed prices;

--- src/features/product/price.js
+++ src/features/product/price.js
@@ -5,48 +5,77 @@ import './styles/price.scss';
 export default function Price({ stock, prices, salePrices }) {
 
     const [qty, setQty] = React.useState(1);
-    const [price, setPrice] = React.useState({});
+    const [currPrice, setCurrPrice] = React.useState(0);
+
+    // function changeHandler(e) {
+    //     const { value } = e.target;
+
+    //     if ( !value ) setQty( 1 );
+    //     else if ( value <= 0  ) {
+    //         e.target.value = 1;
+    //         return setQty( 1 );
+    //     }
+    //     else setQty( value ? value : 1 );
+    // }
+
+    // function findPrice() {
+    //     const price = prices.find( ({ minQty, maxQty }) => {
+    //         if ( maxQty ) {
+    //             return qty >= minQty && qty <= maxQty;
+    //         } else {
+    //             return true;
+    //         }
+    //     });
+
+    //     setPrice(price);
+    // }
 
     function changeHandler(e) {
         const { value } = e.target;
 
-        if ( !value ) setQty( 1 );
-        else if ( value <= 0  ) {
+        if ( value <= 0  ) {
             e.target.value = 1;
             return setQty( 1 );
         }
-        else setQty( value ? value : 1 );
+
+        setQty(value);
     }
 
     function findPrice() {
-        const price = prices.find( ({ minQty, maxQty }) => {
-            if ( maxQty ) {
-                return qty >= minQty && qty <= maxQty;
+        const currPrice = prices.findIndex((price, i) => {
+            if ( prices[i] ) {
+                return price.minQty <= qty && prices[i + 1]?.minQty > qty;
             } else {
-                return true;
+                return prices[i].minQty <= qty;
             }
         });
-
-        setPrice(price);
+        console.log(currPrice)
+        currPrice >= 0 && setCurrPrice(currPrice);
     }
 
     React.useEffect(() => {
-        if ( qty > price.minQty && qty < price.maxQty ) return;
-        else findPrice();
+        findPrice();
     }, [qty]);
 
     return (
         <div className="product-price">
             <div className="product-price_qty">
-                <input onChange={ changeHandler } min="1" max={ stock } placeholder="кол-во (шт)" type="number" inputMode="numeric" />
+                <input 
+                    onChange={ changeHandler } 
+                    min="1" max={ stock } 
+                    value={qty}
+                    placeholder="кол-во (шт)" 
+                    type="number" 
+                    inputMode="numeric" 
+                />
             </div>
 
             <div className="product-price_total">
-                <span>{ `${ price.amount * qty }₽` }</span>
+                <span>{ `${ prices[currPrice].amount * qty }₽` }</span>
             </div>
 
             <div className="product-price_single">
-                <span>{ `${ price.amount }₽/шт` }</span>
+                <span>{ `${ prices[currPrice].amount }₽/шт` }</span>
             </div>
         </div>
     );
